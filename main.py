d3abe666a04f0c4e531c82a03e8ed86a7a6614ea
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2024-11-15 11:49:57 -0800

    cleanup, added portfolio section

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 def layout(*components: AnyComponent, title: str) -> list[AnyComponent]:
     return [
-        # c.PageTitle(text="JONATHAN BOUCHET"),
+        c.PageTitle(text=""),
         c.Navbar(
             title="JONATHAN BOUCHET",
             title_event=GoToEvent(url="/about"),
@@ -32,6 +32,13 @@ def layout(*components: AnyComponent, title: str) -> list[AnyComponent]:
                         ],
                     on_click=GoToEvent(url="/resume"),
                     active="startswith:/resume",
+                ),
+                c.Link(
+                    components=[
+                        c.Markdown(text="Portfolio")
+                        ],
+                    on_click=GoToEvent(url="/data_analysis"),
+                    active="startswith:/data_analysis",
                 ),
                 c.Link(
                     components=[
@@ -120,11 +127,12 @@ Research Scientist
 FACULTÉ DES SCIENCES, UNIVERSITÉ DE NANTES, NANTES, FRANCE
                              
 # Technical Skills
-- Programming Languages: C++ | Python | Shell Scripting | R Programming
+- Programming Languages: Python | R Programming | C++
 - Data analysis: ROOT | NumPy, Scikit | Pandas
 - Software: Linux | UNIX | R studio | Spark
-- Databases: MySQL, PostGreSQL(beginner), MongoDb(beginner)
-- Web development: html, css, bootstrap, Django(beginner), Node(beginner)
+- Databases: SQL(intermediate), MongoDb(intermediate)
+- Front end: React(beginner to intermediate), Vue(beginner to intermediate)
+- Back end: FastAPI, Google GCS
     """), title="resume")
 
 
@@ -134,15 +142,46 @@ def page_contact() -> list[AnyComponent]:
 - [Linkedin](https://www.linkedin.com/in/jonathanbouchet/)
 - [Kaggle](https://www.kaggle.com/jonathanbouchet/)
 - [GitHub](https://github.com/jonathanbouchet)
-- [R Publications](https://rpubs.com/jonathanbouchet)
----
-```py
-def get_sum(a: int, b:int) -> int:
-    '''return the sum a and b
-    '''
-    return a + b
-```                                                                      
+- [R Publications](https://rpubs.com/jonathanbouchet)                                                                 
 """), title="Contact Page!")
+
+
+@app.get("/api/data_analysis",response_model=FastUI, response_model_exclude_none=True)
+def page_contact() -> list[AnyComponent]:
+    return layout(c.Markdown(text="""
+# Kaggle
+A list of selected data analysis published on `Kaggle`
+                                                          
+- [F1 Data analysis](https://www.kaggle.com/code/jonathanbouchet/f1-data-analysis):
+    - data visualization, notebook in `R`                          
+- [U.S. Commercial Flights Tracker Map](https://www.kaggle.com/code/jonathanbouchet/u-s-commercial-flights-tracker-map)
+- data visualization, notebook in `R`  
+- [GOOOOOAAAAALLLLLL !!!!!!!!!!!!](https://www.kaggle.com/code/jonathanbouchet/goooooaaaaallllll):
+    - data visualization, ML for winner prediction, notebook in `R`  
+- [Forecasting Beijing's housing](https://www.kaggle.com/code/jonathanbouchet/forecasting-beijing-s-housing):
+    - data visualization, ML for price of House, notebook in `R`
+- [Age of empire Winner prediction](https://www.kaggle.com/code/jonathanbouchet/base-dataset-winner-s-prediction):
+    - data visualization, ML for AOE's games winner, ensemble of tuned classifiers, notebook in `python`
+- [Recommendation systems for MetaQuest market places](https://www.kaggle.com/code/jonathanbouchet/recommendation-systems-for-metaquest-market-places):
+    - data visualization, Recommendation systems, NLP, content based and collaborative filtering, notebook in `python`
+- [League Of Legends data: 2 days snapshot on Korean server](https://www.kaggle.com/code/jonathanbouchet/lol-data-2-days-snapshot-on-kr-server):
+    - data visualization, time series, LSTM, Keras, notebook in `python` 
+- [Genshin Impact(patch 3.1) Voice lines analysis](https://www.kaggle.com/code/jonathanbouchet/genshin-impact-patch-3-1-voice-lines-analysis):
+    - data visualization, NLP, Topic Modelling, notebook in `python`  
+                             
+# Apps.
+                             
+- [Pokemon App](https://jonathanbouchet.shinyapps.io/pokeData/):
+    - mean characteristics of Pokemon (gen 1-6) with radarcharts, `R shiny`, data analysis 
+- [Cities Transportation System](https://jonathanbouchet.shinyapps.io/transport_visualization/):
+    - Visualization and historical data plots of Transportayion System from several world cities, data from Kaggle, via citylines.co data, `R shiny`, data analysis  
+                             
+# Scientific Publications
+- [Identification of charmed mesons using Multivariate analysis in STAR](https://iopscience.iop.org/article/10.1088/1742-6596/396/2/022007/meta)
+- [Charmed decay reconstruction using a microvertexing technique with the STAR Silicon Detectors](https://pos.sissa.it/cgi-bin/reader/contribution.cgi?id=135/031)
+- [Heavy Flavor Tracker (HFT) : A new inner tracking device at STAR](https://arxiv.org/abs/0907.3407)
+                                                                                                                                   
+"""), title="portfolio")
 
 
 @app.get("/{path:path}")
